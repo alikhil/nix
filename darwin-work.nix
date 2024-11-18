db0b12ab813c4a19fc808b74aa752b8aacc291d7
Alik Khilazhev <a.khilazhev@criteo.com>
2024-11-18 10:43:35 +0100

    wip

---
+++ darwin-work.nix
@@ -0,0 +1,97 @@
+{ self, nix-darwin, pkgs, nixpkgs, nix-homebrew, homebrew-core, homebrew-cask, ... }: {
+
+      nixpkgs.config.allowUnfree = true;
+
+      homebrew = {
+        enable = true;
+        casks = [
+          "betterdisplay"
+          "deepl"
+        ];
+        brews = [
+          "colima"
+          "mas" # for querying the Mac App Store: mas search "Singularity"
+          "python"
+          "libpq"
+        ];
+
+        masApps = {
+          "Singularity" = 1481535767;
+          "Xcode" = 497799835;
+          "Wireguard" = 1451685025;
+          "1Password for Safari" = 1569813296;
+        };
+
+
+        onActivation.cleanup = "zap";
+        onActivation.autoUpdate = true;
+        onActivation.upgrade = true;
+      };
+
+      # List packages installed in system profile. To search by name, run:
+      # $ nix-env -qaP | grep wget
+      environment.systemPackages =
+        with pkgs; [
+          tmux
+          git
+          watch
+          docker
+          docker-compose
+          wget
+          iterm2
+          jq
+          inetutils # telnet
+          kubectl
+          iperf3
+          nmap
+          pre-commit
+          gitleaks
+          devenv
+          nixpkgs-fmt
+	  vscode
+        ];
+
+      # darwin-help, mynixos.com
+      system.defaults = {
+        # dock.persistent-apps = [
+        #   "/System/Library/CoreServices/Finder.app"
+        #   "/System/Applications/Calendar.app"
+        #   "/Applications/Firefox.app"
+        #   "/System/Applications/Mail.app"
+        #   "/Applications/SingularityApp.app"
+        #   "${pkgs.iterm2}/Applications/iTerm2.app"
+        #   "${pkgs.obsidian}/Applications/Obsidian.app"
+        #   "${pkgs.telegram-desktop}/Applications/Telegram.app"
+        # ];
+        finder.FXPreferredViewStyle = "clmv";
+        loginwindow.GuestEnabled = false;
+
+      };
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      # system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 5;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users."a.khilazhev" = {
+        name = "a.khilazhev";
+        home = "/Users/a.khilazhev";
+    };
+
+}
--- flake.nix
+++ flake.nix
@@ -63,6 +63,44 @@
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."mini".pkgs;
+    # darwinPackages = self.darwinConfigurations."mini".pkgs;
+    darwinConfigurations."VDF4JP6D5V" = nix-darwin.lib.darwinSystem {
+    system = "aarch64-darwin";
+     modules = [
+        ./darwin-work.nix
+        home-manager.darwinModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users."a.khilazhev" = import ./home.nix;
+
+          # Optionally, use home-manager.extraSpecialArgs to pass
+          # arguments to home.nix
+        }
+        nix-homebrew.darwinModules.nix-homebrew
+        {
+          nix-homebrew = {
+            # Install Homebrew under the default prefix
+            enable = true;
+
+            # Apple Silicon Only: Also install Homebrew under the default Intel prefix for Rosetta 2
+            enableRosetta = true;
+
+            # User owning the Homebrew prefix
+            user = "a.khilazhev";
+
+            taps = {
+              "homebrew/homebrew-core" = homebrew-core;
+              "homebrew/homebrew-cask" = homebrew-cask;
+            };
+
+            # Automatically migrate existing Homebrew installations
+            autoMigrate = true;
+          };
+        }
+      ];
+   };
+
+    darwinPackages = self.darwinConfigurations."VDF4JP6D5V".pkgs +  self.darwinConfigurations."mini".pkgs;
   };
 }
