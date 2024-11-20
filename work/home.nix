ce144f0c8a5f5ac75418494bdeced849316d51ec
Alik Khilazhev <a.khilazhev@criteo.com>
2024-11-20 13:51:24 +0100

    wip

--- work/home.nix
+++ work/home.nix
@@ -39,16 +39,25 @@
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
   # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
+    ".colima/_lima/_config/override.yaml".text = ''
+      # mountType: 9p
+      # mounts:
+      # - location: "/Users/a.khilazhev"
+      #   writable: true
+      #   9p:
+      #     securityModel: mapped-xattr
+      #     cache: mmap
+      # - location: "~"
+      #   writable: true
+      #   9p:
+      #     securityModel: mapped-xattr
+      #     cache: mmap
+      # - location: /tmp/colima
+      #   writable: true
+      #   9p:
+      #     securityModel: mapped-xattr
+      #     cache: mmap
+    '';
   };
 
   # You can also manage environment variables but you will have to manually
@@ -188,6 +197,7 @@
       zhuangtongfa.material-theme
       zxh404.vscode-proto3
       bbenoist.nix
+      puppet.puppet-vscode
     ];
     userSettings =
       builtins.fromJSON (builtins.readFile ../common/vscode/settings.json) //
