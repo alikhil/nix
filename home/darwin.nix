1edb637cb91c9b30f6bbae91ce8f37c054ec5eff
Alik Khilazhev <git@alik.page>
2025-01-18 16:47:09 +0100

    add parcel app

--- home/darwin.nix
+++ home/darwin.nix
@@ -25,6 +25,7 @@
       "Ethernet Status" = 1186187538;
       "iMovie" = 408981434;
       "Wireguard" = 1451685025;
+      "Parcel" = 639968404;
       # "1Password for Safari" = 1569813296;
     };
 
